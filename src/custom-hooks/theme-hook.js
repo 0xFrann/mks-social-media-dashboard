c7f659058176aeb159d74bad3f28993057c7ef24
Franco Dalmasso <francodalmasso@gmail.com>
2020-07-24 00:39:53 -0300

    Container, Grid, Flex and Display utilities. Refactor SASS

--- src/custom-hooks/theme-hook.js
+++ src/custom-hooks/theme-hook.js
@@ -1,17 +1,21 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 export default () => {
+  const isMountRef = useRef(true);
   const [theme, setTheme] = useState(false);
   const toggleTheme = () => setTheme(!theme);
 
   useEffect(() => {
-    window.document.body.classList.toggle("dark-theme");
+    if (isMountRef.current) {
+      isMountRef.current = false;
+    } else {
+      window.document.body.classList.toggle("dark-theme");
+    }
   }, [theme]);
 
   // Detect device theme conf
   useEffect(() => {
     if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) {
-      window.document.body.classList += "dark-theme";
       toggleTheme();
     }
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
